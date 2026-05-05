c7fd3a1612a62fcad7eb761a4e9526aa4b5c5369
Egg-03 <111327101+eggy03@users.noreply.github.com>
2026-05-05 12:03:59 +0530

    chore: update author and about text

--- src/main/java/io/github/eggy03/application/constant/VersionAndOtherInfo.java
+++ src/main/java/io/github/eggy03/application/constant/VersionAndOtherInfo.java
@@ -1,39 +1,32 @@
 package io.github.eggy03.application.constant;
 
+@SuppressWarnings("unused")
 public class VersionAndOtherInfo {
 
-    public static final String APP_AUTHOR = "Egg-03";
+    public static final String APP_AUTHOR = "Sayan Bhattacharya";
     public static final String APP_VERSION = "2.0.0-alpha1";
     public static final String LICENSE3J_AUTHOR = "Peter Verhas";
     public static final String LICENSE3J_VERSION = "3.3.0";
     public static final String ABOUT = """
             <h2>About License3J GUI</h2>
             <p>
-            License3J GUI is a Graphical User Interface developed by Egg-03 for the free and open source License3j library.
-            The source code is based on the License3j-REPL Application. This application provides a user-friendly interface
-            to create, view, and validate license files, generate keys for signing the licenses, using the powerful features
-            of the License3j library.
+            License3J GUI is a Graphical User Interface developed for the free and open source License3j library.
+            The design and functionality of this app is heavily inspired from the License3j-REPL Application.
+            It is recommended that you check out the REPL Application and read a bit about License3j itself,
+            to have an understanding of how to use this application.
             </p>
-            
-            <h2>About License3J</h2>
             <p>
-            License3j is a free and open-source Java library to manage license files in Java programs.
-            A license file is a special electronically signed configuration file. The library can create
-            (including signing), read, and verify such license files.
-            </p>
+            License3j GUI binaries are not signed. Please be careful when downloading from unofficial sources.
             
-            <h2>Licensing</h2>
-            <p>
-            Both License3J and the GUI application are licensed under the Apache 2.0 License.
-            </p>
-            
-            <h2>Other Libraries Used</h2>
+            <h2>Libraries Used</h2>
             <ul>
+                <li>License3j</li>
+                <li>SLF4J</li>
                 <li>TinyLog</li>
                 <li>MigLayout</li>
-                <li>Apache Commons IO</li>
                 <li>FlatLaf</li>
-                <li>Flexmark</li>
+                <li>Apache Commons IO</li>
+                <li>JSpecify</li>
             </ul>
             """;
 
